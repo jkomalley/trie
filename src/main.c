aa8e36c46aedf980f903737f32954322c9744330
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2024-01-26 17:00:31 +0000

    add use of delete function

--- src/main.c
+++ src/main.c
@@ -18,6 +18,20 @@ int main()
         printf("ball not found :(\n");
     }
 
+    bool ret = delete(root, "ball", 5);
+
+    if(ret){
+        printf("delete successful\n");
+    } else {
+        printf("delete not successful\n");
+    }
+
+    if(search(root, "ball", 5)){
+        printf("ball found :)\n");
+    } else {
+        printf("ball not found :(\n");
+    }
+
     freeTrie(root);
 
     return 0;
